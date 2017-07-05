ef9d75e48ec8f1d691c3b2e03fc5907ecbb1d5f8
Valentina1425 <valentina.rodriguez1425@gmail.com>
2017-07-05 12:55:29 -0400

    añado arreglo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,4 @@
-/**/
-/*El siguiente fragmento de código te mostrará la versión básica del SDK, con las opciones
- configuradas en sus valores predeterminados más comunes.*/
-window.fbAsyncInit = function() {  
-    FB.init({
-      appId      : '149453778947493',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.8'
-    });
-    FB.AppEvents.logPageView();   
-  };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-/*------------------------------------*/
 /*El primer paso al cargar tu página web es determinar si una persona ya inició sesión 
 en tu aplicación con el inicio de sesión con Facebook. Este proceso comienza con una 
 llamada a FB.getLoginStatus. Esta función activa una llamada a Facebook para obtener el estado de 
@@ -48,3 +27,5 @@ function checkLoginState() {
     statusChangeCallback(response);
   });
 }
+
+
